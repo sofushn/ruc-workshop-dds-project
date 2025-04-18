10ab7985654a3438fd7ccb12df648544dd1e794a
MathiasHJ <mathiashj@ruc.dk>
2025-04-18 16:33:30 +0200

    Update build_image_db.sql

--- database/build_image_db.sql
+++ database/build_image_db.sql
@@ -1,13 +1,19 @@
 DROP TABLE IF EXISTS image;
 
 create table image (
-id INT PRIMARY KEY, 
-x DECIMAL(38,10) not null,
-y DECIMAL(38,10) not null,
+id INT IDENTITY PRIMARY KEY, 
+x DECIMAL(8,4) not null,
+y DECIMAL(8,4) not null,
 reference_to_image_store varchar(255) not null,
 relation_to_other_images varchar (255) not null
 );
 
 
-INSERT INTO image (id,x,y,reference_to_image_store,relation_to_other_images) VALUES 
-(1,1201.2001,2354.4351,'test1','test1')
+INSERT INTO image (x,y,reference_to_image_store,relation_to_other_images) VALUES 
+(1201.2001,2354.4351,'test1','test1')
+
+INSERT INTO image (x,y,reference_to_image_store,relation_to_other_images) VALUES 
+(1201.2001,2354.4351,'test2','test2')
+
+select* from image
+
