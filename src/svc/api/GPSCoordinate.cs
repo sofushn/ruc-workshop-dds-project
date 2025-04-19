31f816c89b9b0643fd5ddb27b3e768b034490c89
SleepDeprivedLychee <jhr.liang@gmail.com>
2025-04-19 23:49:24 +0200

    Added CRUD Operations and In-memory store to API

---
+++ src/svc/api/GPSCoordinate.cs
@@ -0,0 +1,7 @@
+namespace Api;
+
+public class GPSCoordinate {
+    public required string ImageId {get; set;}
+    public required double X { get; set; }
+    public required double Y { get; set; }
+}
--- src/svc/api/Program.cs
+++ src/svc/api/Program.cs
@@ -1,41 +1,26 @@
+using Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<CoordinateStore>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/coordinates", CoordinatesHandler.GetAll);
+app.MapGet("/coordinates/{imageId}", CoordinatesHandler.GetById);
+app.MapPost("/coordinates", CoordinatesHandler.Create);
+app.MapPut("/coordinates/{imageId}", CoordinatesHandler.Update);
+app.MapDelete("/coordinates/{imageId}", CoordinatesHandler.Delete);
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
