feb213850261589e1f2e5dea334ef0b238373ab2
MathiasHJ <mathiashj@ruc.dk>
2025-04-21 15:51:50 +0200

    updated table values
    changed x & y to latitude & longitude
    added height to the table

--- database/build_image_db.sql
+++ database/build_image_db.sql
@@ -1,19 +1,21 @@
 DROP TABLE IF EXISTS image;
 
+
 create table image (
 id INT IDENTITY PRIMARY KEY, 
-x DECIMAL(8,4) not null,
-y DECIMAL(8,4) not null,
+longitude DECIMAL(15,14) not null,
+latitude DECIMAL(18,16) not null,
+height DECIMAL(4,2) not null,
 reference_to_image_store varchar(255) not null,
 relation_to_other_images varchar (255) not null
 );
 
 
-INSERT INTO image (x,y,reference_to_image_store,relation_to_other_images) VALUES 
-(1201.2001,2354.4351,'test1','test1')
+INSERT INTO image (longitude,latitude,height,reference_to_image_store,relation_to_other_images) VALUES 
+(1.20000000000001,23.4300000000000051,70.08,'test1','test1')
 
-INSERT INTO image (x,y,reference_to_image_store,relation_to_other_images) VALUES 
-(1201.2001,2354.4351,'test2','test2')
+INSERT INTO image (longitude,latitude,height,reference_to_image_store,relation_to_other_images) VALUES 
+(1.20000000000001,23.4300000000000051,50.56,'test2','test2')
 
 --select* from image
 
