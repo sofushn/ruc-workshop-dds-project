cc3df77dba2a6c740a32cc42f360dabd80517563
MathiasHJ <mathiashj@ruc.dk>
2025-04-24 19:29:55 +0200

    Dockerfile setup

--- src/svc/database/build_image_db.sql
+++ src/svc/database/build_image_db.sql
@@ -1,35 +1,36 @@
 DROP TABLE IF EXISTS image;
 
-
-create table waypoint (
-    id INT IDENTITY PRIMARY KEY, 
-    longitude DECIMAL(15,14) NOT NULL UNIQUE,
-    latitude DECIMAL(18,16) NOT NULL UNIQUE,
-    height DECIMAL(4,2) NOT NULL,
-    image_id VARCHAR(255) NOT NULL
-
-);
-
 create table map (
-    id INT IDENTITY PRIMARY KEY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     NW_longitude DECIMAL(15,14) NOT NULL UNIQUE,
     NW_latitude DECIMAL(18,16) NOT NULL UNIQUE,
     SE_longitude DECIMAL(15,14) NOT NULL UNIQUE,
     SE_latitude DECIMAL(18,16) NOT NULL UNIQUE,
     image_id VARCHAR(255) NOT NULL
 
-)
+);
+
+create table waypoint (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+    longitude DECIMAL(15,14) NOT NULL UNIQUE,
+    latitude DECIMAL(18,16) NOT NULL UNIQUE,
+    height DECIMAL(4,2) NOT NULL,
+    image_id VARCHAR(255) NOT NULL,
+    map_id INT REFERENCES map(id) NOT NULL
+
+);
 
 
-
-INSERT INTO image (longitude,latitude,height,reference_to_image_store,relation_to_other_images) VALUES 
-(1.20000000000001,23.4300000000000051,70.08,'test1','test1')
-
-INSERT INTO image (longitude,latitude,height,reference_to_image_store,relation_to_other_images) VALUES 
-(1.20000000000001,23.4300000000000051,50.56,'test2','test2')
 
 INSERT INTO map (NW_longitude, NW_latitude, SE_longitude, SE_latitude, image_id) VALUES
-(1.20000000000001,23.4300000000000051,9.40000000000002,73.5700000000000051)
+(1.20000000000001,23.4300000000000051,9.40000000000002,73.5700000000000051,'test1');
 
---select* from image
+INSERT INTO waypoint (longitude,latitude,height,image_id,map_id) VALUES 
+ (1.20000000000011,23.4300000000000321,70.08,'test1',1);
+
+INSERT INTO waypoint (longitude,latitude,height,image_id,map_id) VALUES 
+ (1.20000000000001,23.4300000000000051,50.56,'test2',1);
+
+
+
 
