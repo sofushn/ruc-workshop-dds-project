cdfbc11014e85543dc4b68a9cc98d23b55be947f
SleepDepivedLychee <118348197+SleepDepivedLychee@users.noreply.github.com>
2025-04-28 17:41:32 +0200

    feat: api docker (#31)
    * Added dummy data to test
    
    * Added Dockerfile, Dockerignore and updated api.csproj for docker.
    
    * Changed small a to A in Api.csproj and in Docker

--- src/svc/api/Program.cs
+++ src/svc/api/Program.cs
@@ -30,4 +30,35 @@ app.MapPost("/coordinates", CoordinatesHandler.Create);
 app.MapPut("/coordinates/{imageId}", CoordinatesHandler.Update);
 app.MapDelete("/coordinates/{imageId}", CoordinatesHandler.Delete);
 
+// Add dummy data to the in-memory store, remove later.
+var coordinateStore = app.Services.GetRequiredService<CoordinateStore>();
+
+coordinateStore.Add(new GPSCoordinate
+{
+    ImageId = "image-01",
+    X = 12.34,
+    Y = 56.78
+});
+
+coordinateStore.Add(new GPSCoordinate
+{
+    ImageId = "image-02",
+    X = 98.76,
+    Y = 54.32
+});
+
+coordinateStore.Add(new GPSCoordinate
+{
+    ImageId = "image-03",
+    X = 66.81,
+    Y = 32.44
+});
+
+coordinateStore.Add(new GPSCoordinate
+{
+    ImageId = "image-04",
+    X = 58.06,
+    Y = 74.28
+});
+
 app.Run();
