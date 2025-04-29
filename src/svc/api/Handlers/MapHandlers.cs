de2ac584259c62eec7793bc52da4e12273fe6574
MathiasHJ <mathiashj@ruc.dk>
2025-04-29 16:15:07 +0200

    feat: created MapHandler

---
+++ src/svc/api/Handlers/MapHandlers.cs
@@ -0,0 +1,16 @@
+﻿
+using Api.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Handlers;
+
+public class MapHandlers
+{
+
+    public static IResult GetById(int mapId, [FromServices] Context.Context context)
+    {
+        var map = context.Maps.FirstOrDefault(m => m.Id == mapId);
+        return map != null ? Results.Ok(map) : Results.NotFound();
+    } 
+}
