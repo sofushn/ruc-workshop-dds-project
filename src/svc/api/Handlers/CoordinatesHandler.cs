131c79393d0355be9c1896889b6c419a9ec72cae
Sofus Hilfling <sofus@techchapter.com>
2025-04-30 14:30:19 +0200

    fix: invalid type name

--- src/svc/api/Handlers/CoordinatesHandler.cs
+++ src/svc/api/Handlers/CoordinatesHandler.cs
@@ -4,23 +4,23 @@ using Microsoft.EntityFrameworkCore;
 namespace Api;
 
 public class CoordinatesHandler {
-    public static IResult GetAll(int mapId, [FromServices] Context.Context context)
+    public static IResult GetAll(int mapId, [FromServices] Context context)
         => Results.Ok(context.GPSCoordinates.Where(x => x.MapId == mapId).AsNoTracking());
     
-    public static IResult GetById(int coordinateId, [FromServices] Context.Context context)
+    public static IResult GetById(int coordinateId, [FromServices] Context context)
     {
-        var coord = context.GPSCoordinates.FirstOrDefault(x => x.Id == coordinateId);
+        var coord = GPSCoordinates.FirstOrDefault(x => x.Id == coordinateId);
         return coord is not null ? Results.Ok(coord) : Results.NotFound();
     }
 
-    public static IResult Create(GPSCoordinate coordinate, [FromServices] Context.Context context)
+    public static IResult Create(GPSCoordinate coordinate, [FromServices] Context context)
     {
         context.GPSCoordinates.Add(coordinate);
         context.SaveChanges();
         return Results.Created($"/coordinates/{coordinate.ImageId}", coordinate);
     }
 
-    public static IResult Update(int coordinateId, GPSCoordinate updated, [FromServices] Context.Context context)
+    public static IResult Update(int coordinateId, GPSCoordinate updated, [FromServices] Context context)
     {
         var coord = context.GPSCoordinates.FirstOrDefault(x => x.Id == coordinateId);
         if (coord == null) return Results.NotFound();
@@ -30,7 +30,7 @@ public class CoordinatesHandler {
 
         return Results.Ok(updated);    
     }
-    public static IResult Delete(int coordinateId, [FromServices] Context.Context context)
+    public static IResult Delete(int coordinateId, [FromServices] Context context)
     {
         var coord = context.GPSCoordinates.FirstOrDefault(x => x.Id == coordinateId);
         if (coord == null) return Results.NotFound();
--- src/svc/api/Handlers/MapHandler.cs
+++ src/svc/api/Handlers/MapHandler.cs
@@ -6,9 +6,9 @@ namespace Api;
 public class MapHandler
 {
 
-    public static IResult GetAll([FromServices] Context.Context context)
+    public static IResult GetAll([FromServices] Context context)
       => Results.Ok(context.Maps.AsNoTracking());
-    public static IResult GetById(int mapId, [FromServices] Context.Context context)
+    public static IResult GetById(int mapId, [FromServices] Context context)
     {
         var map = context.Maps.FirstOrDefault(m => m.Id == mapId);
         return map != null ? Results.Ok(map) : Results.NotFound();
