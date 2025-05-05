12502bdd3c3fb1a576ff1e1d96f655864d210f6c
mkmason <78545722+mkmason@users.noreply.github.com>
2025-05-06 00:24:13 +0200

    fix: add Map Id input and update API endpoint for fetching map data

--- src/svc/website/src/index.js
+++ src/svc/website/src/index.js
@@ -1,18 +1,21 @@
 let imageHostUrl = "http://localhost:8080/image-api/images/";
-let apiHostUrl = "http://localhost:5165/coordinates/";
+let apiHostUrl = "http://localhost:8080/api/";
 let mapImage = document.getElementById('mapImage');
-mapImage.src = imageHostUrl+"2dmap.jpg";
 
-let mapBoundaries = {
-    northEast: { lat: 55.655294, lng: 12.144353 },
-    southWest: { lat: 55.651107, lng: 12.134139 },
-};
 
 let panorama = pannellum.viewer('panorama', {
     "type": "equirectangular",
     "autoLoad": true,
     "showControls": false,
 });
+
+function createMap(data) {
+mapImage.src = imageHostUrl + data.imageId + ".jpg";
+mapBoundaries = {
+    northEast: { lat: data.neLatitude, lng: data.neLongitude },
+    southWest: { lat: data.swLatitude, lng: data.swLongitude },
+};
+}
 
 function updateMap(data) {
     data.forEach((location, index) => {
@@ -23,9 +26,9 @@ function updateMap(data) {
         button.style.height = '10px';
         button.style.backgroundColor = 'red';
         button.style.borderRadius = '50%';
-        button.style.left = ((location.x - mapBoundaries.southWest.lng) / 
+        button.style.left = ((location.longitude - mapBoundaries.southWest.lng) / 
                             (mapBoundaries.northEast.lng - mapBoundaries.southWest.lng) * 100) + '%';
-        button.style.top = ((mapBoundaries.northEast.lat - location.y) / 
+        button.style.top = ((mapBoundaries.northEast.lat - location.latitude) / 
                             (mapBoundaries.northEast.lat - mapBoundaries.southWest.lat) * 100) + '%';
         button.style.transform = 'translate(-50%, -50%)';
         button.style.zIndex = 1000;
@@ -48,15 +51,18 @@ function postImageData()
     let lat = document.getElementById("latitude").value;
     let lng = document.getElementById("longitude").value;
     let image_id = document.getElementById("image_id").value;
-    fetch(apiHostUrl, {
+    let mapid = document.getElementById("coordinateMapId").value;
+    let fetchUrl = apiHostUrl +"/coordinates/3";
+    fetch(fetchUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             imageId: image_id,
-            x: parseFloat(lng),
-            y: parseFloat(lat)
+            latitude: parseFloat(lat),
+            longitude: parseFloat(lng),
+            mapId: mapid
         })
     })
     .then(response => {
@@ -72,8 +78,33 @@ function postImageData()
         console.error('Error:', error);
     });
 }
-function getImageData(){
-    fetch(apiHostUrl, {
+function getMap() {
+    let mapid = document.getElementById("mapId").value;
+    let fetchUrl = apiHostUrl + "map/" + mapid;
+    fetch(fetchUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        createMap(data);
+        console.log('Success:', data);
+        getMapData(mapid);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+function getMapData(mapid){
+    let fetchUrl = apiHostUrl + "map/" + mapid + "/coordinates";
+    fetch(fetchUrl, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -91,50 +122,5 @@ function getImageData(){
     })
     .catch(error => {
         console.error('Error:', error);
-    });
-}
-
-function postPlaceholderData(){
-    let locationData = [{
-        "latitude": 55.651459,
-        "longitude": 12.134499,
-        "image_id": "1"
-    }, {
-        "latitude": 55.653372,
-        "longitude": 12.140358,
-        "image_id": "2"
-    }, {
-        "latitude": 55.652658,
-        "longitude": 12.139993,
-        "image_id": "3"
-    }, {
-        "latitude": 55.654111,
-        "longitude": 12.138877,
-        "image_id": "4"
-    }];
-    locationData.forEach(location => {
-        fetch(apiHostUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                imageId: location.image_id,
-                x: parseFloat(location.longitude),
-                y: parseFloat(location.latitude)
-            })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
     });
 }
