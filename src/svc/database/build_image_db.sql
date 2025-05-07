18a5f9864c3a97b2517f7be2a18dd6128e7c3c0b
mkmason <78545722+mkmason@users.noreply.github.com>
2025-05-07 22:27:26 +0200

    fix: correct decimal precision and update insert statements in build_image_db.sql (#54)

--- src/svc/database/build_image_db.sql
+++ src/svc/database/build_image_db.sql
@@ -2,18 +2,18 @@ DROP TABLE IF EXISTS image;
 
 create table map (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    NE_longitude DECIMAL(15,14) NOT NULL UNIQUE,
-    NE_latitude DECIMAL(18,16) NOT NULL UNIQUE,
-    SW_longitude DECIMAL(15,14) NOT NULL UNIQUE,
-    SW_latitude DECIMAL(18,16) NOT NULL UNIQUE,
+    NE_longitude DECIMAL(18,15) NOT NULL UNIQUE,
+    NE_latitude DECIMAL(18,15) NOT NULL UNIQUE,
+    SW_longitude DECIMAL(18,15) NOT NULL UNIQUE,
+    SW_latitude DECIMAL(18,15) NOT NULL UNIQUE,
     image_id VARCHAR(255) NOT NULL
 
 );
 
 create table waypoint (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
-    longitude DECIMAL(15,14) NOT NULL UNIQUE,
-    latitude DECIMAL(18,16) NOT NULL UNIQUE,
+    longitude DECIMAL(18,15) NOT NULL UNIQUE,
+    latitude DECIMAL(18,15) NOT NULL UNIQUE,
     height DECIMAL(4,2) NOT NULL,
     image_id VARCHAR(255) NOT NULL,
     map_id INT REFERENCES map(id) NOT NULL
@@ -22,14 +22,14 @@ create table waypoint (
 
 
 
-INSERT INTO map (NW_longitude, NW_latitude, SE_longitude, SE_latitude, image_id) VALUES
-(1.20000000000001,23.4300000000000051,9.40000000000002,73.5700000000000051,'test1');
+INSERT INTO map (NE_longitude, NE_latitude, SW_longitude, SW_latitude, image_id) VALUES
+(12.144353,55.655294,12.134139,55.651107,'test1');
 
 INSERT INTO waypoint (longitude,latitude,height,image_id,map_id) VALUES 
- (1.20000000000011,23.4300000000000321,70.08,'test1',1);
+ (12.140358,55.653372,70.08,'1',1);
 
 INSERT INTO waypoint (longitude,latitude,height,image_id,map_id) VALUES 
- (1.20000000000001,23.4300000000000051,50.56,'test2',1);
+ (12.134499,55.651459,50.56,'2',1);
 
 
 
