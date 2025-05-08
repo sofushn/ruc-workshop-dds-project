0b72d5d1929eb6ce005cec86ec6c0d233f7b5e54
Sofus Hilfling <sofus@techchapter.com>
2025-05-08 15:13:34 +0200

    fix: unique constraints  in db

--- src/svc/database/build_image_db.sql
+++ src/svc/database/build_image_db.sql
@@ -2,21 +2,22 @@ DROP TABLE IF EXISTS image;
 
 create table map (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    ne_longitude DECIMAL(18,15) NOT NULL UNIQUE,
-    ne_latitude DECIMAL(18,15) NOT NULL UNIQUE,
-    sw_longitude DECIMAL(18,15) NOT NULL UNIQUE,
-    sw_latitude DECIMAL(18,15) NOT NULL UNIQUE,
-    image_id VARCHAR(255) NOT NULL
-
+    ne_longitude DECIMAL(18,15) NOT NULL,
+    ne_latitude DECIMAL(18,15) NOT NULL,
+    sw_longitude DECIMAL(18,15) NOT NULL,
+    sw_latitude DECIMAL(18,15) NOT NULL,
+    image_id VARCHAR(255) NOT NULL,
+    UNIQUE (ne_longitude, ne_latitude, sw_longitude, sw_latitude)
 );
 
 create table waypoint (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
-    longitude DECIMAL(18,15) NOT NULL UNIQUE,
-    latitude DECIMAL(18,15) NOT NULL UNIQUE,
+    longitude DECIMAL(18,15) NOT NULL,
+    latitude DECIMAL(18,15) NOT NULL,
     height DECIMAL(4,2) NOT NULL,
     image_id VARCHAR(255) NOT NULL,
-    map_id INT REFERENCES map(id) NOT NULL
+    map_id INT REFERENCES map(id) NOT NULL,
+    UNIQUE (longitude, latitude)
 
 );
 
