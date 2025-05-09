87d4e04d32b0e922cf087b909db236c353386da7
mkmason <78545722+mkmason@users.noreply.github.com>
2025-05-09 19:00:34 +0200

    fix: frontend backed connected in k8s (#55)
    * fix: add Map Id input and update API endpoint for fetching map data
    
    * fix: update waypoint table to allow nullable height and add new waypoint entries; enhance map boundary checks in updateMap function. fix posting coordinates
    
    * fix: unique constraints  in db
    
    ---------
    
    Co-authored-by: Sofus Hilfling <sofus@techchapter.com>

--- src/svc/database/build_image_db.sql
+++ src/svc/database/build_image_db.sql
@@ -2,21 +2,22 @@ DROP TABLE IF EXISTS image;
 
 create table map (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    ne_longitude DECIMAL(18,15) NOT NULL UNIQUE,
-    ne_latitude DECIMAL(18,15) NOT NULL UNIQUE,
-    sw_longitude DECIMAL(18,15) NOT NULL UNIQUE,
-    sw_latitude DECIMAL(18,15) NOT NULL UNIQUE,
-    image_id VARCHAR(255) NOT NULL
-
+    ne_longitude DECIMAL(18,15) NOT NULL,
+    ne_latitude DECIMAL(18,15) NOT NULL,
+    sw_longitude DECIMAL(18,15) NOT NULL,
+    sw_latitude DECIMAL(18,15) NOT NULL,
+    image_id VARCHAR(255) NOT NULL,
+    UNIQUE (ne_longitude, ne_latitude, sw_longitude, sw_latitude)
 );
 
 create table waypoint (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
-    longitude DECIMAL(18,15) NOT NULL UNIQUE,
-    latitude DECIMAL(18,15) NOT NULL UNIQUE,
+    longitude DECIMAL(18,15) NOT NULL,
+    latitude DECIMAL(18,15) NOT NULL,
     height DECIMAL(4,2) NOT NULL,
     image_id VARCHAR(255) NOT NULL,
-    map_id INT REFERENCES map(id) NOT NULL
+    map_id INT REFERENCES map(id) NOT NULL,
+    UNIQUE (longitude, latitude)
 
 );
 
@@ -31,6 +32,8 @@ INSERT INTO waypoint (longitude,latitude,height,image_id,map_id) VALUES
 INSERT INTO waypoint (longitude,latitude,height,image_id,map_id) VALUES 
  (12.134499,55.651459,50.56,'2',1);
 
+ INSERT INTO waypoint (longitude,latitude,height,image_id,map_id) VALUES 
+ (12.139993,55.652658,50.56,'3',1);
 
-
-
+ INSERT INTO waypoint (longitude,latitude,height,image_id,map_id) VALUES 
+ (12.138877,55.654111,50.56,'4',1);
