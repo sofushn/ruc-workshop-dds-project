b6e6dfb2b407da148e4786741ed923181983c70a
JacobDNielsen <jacobpeternielsen@gmail.com>
2025-05-12 18:28:29 +0200

    feat: updated k6 test logic for img-store breakpoint-test

--- src/k6/image-store/README.md
+++ src/k6/image-store/README.md
@@ -5,3 +5,7 @@
 * soak-test.js = 4 test. Purpose is to test how system performs over prolonged time of traffic load. Is reusing avg-load.js but with prolonged duration of 3 hours
 * spike-test.js = 5 test. Purpose is to direct massive amount of traffic in short amount of time
 * breakpoint-test.js = 6 test. Purpose is to find out when system breaks
+
+
+Safe test result for breakpoint-test.js (imagestore)
+$timestamp = Get-Date -Format "yyyyMMdd-HHmmss"; if (!(Test-Path -Path "./results")) { New-Item -ItemType Directory -Path "./results" }; k6 run --summary-export="results/summary-$timestamp.json" --out csv="results/results-$timestamp.csv" ./breakpoint-test.js | Tee-Object -FilePath "results/output-$timestamp.txt"
--- src/k6/image-store/breakpoint-test.js
+++ src/k6/image-store/breakpoint-test.js
@@ -1,31 +1,56 @@
-import { check, group, sleep } from "k6";
+import { check, group } from "k6";
 import http from "k6/http";
+
+let getCount = 0;
+const fileData = open("Trollface.jpg", "b");
 
 export const options = {
     executor: "ramping-arrival-rate", 
     stages: [
         { duration: "2h", target: 10000 },
     ],
+    gracefulStop: "1m",
+
+    thresholds: {
+        "http_req_duration": [
+            { threshold: "p(90)<500", abortOnFail: true, delayAbortVal: "20s" },
+            { threshold: "p(95)<800", abortOnFail: true, delayAbortEval: "20s" },
+        ], 
+        "http_req_failed": [{ threshold: "rate<0.01", abortOnFail: true, delayAbortVal: "20s" }],
+        // "checks": ["rate>0.90"], 
+    },
 
 };
+
 export default function () {
     group("/images", () => {
-        const response = http.get("http://localhost:8080/image-api/images/");
+        const response = http.get("http://localhost:8080/image-api/images");
 
         check(response, {
             "status is 200": (r) => r.status === 200,
         });
-
-    sleep(Math.random() * 4 + 1);
+        getCount++;
     });
 
     group("/3.jpg", () => {
-        const response = http.get("http://localhost:8080/image-api/images/3.jpg");
+        const response = http.get("http://localhost:8080/image-api/images/637d41bf-24e1-4ba5-87ef-33007f188316.jpg");
         check(response, {
             "status is 200": (r) => r.status === 200,
         });
-
-    sleep(Math.random() * 4 + 1);
+        getCount++;
     });
 
+    if (getCount > 1000) {
+    group("PostImg", () => {
+        const data = {
+            file: http.file(fileData, "Trollface.jpg"),
+        };
+
+        const response = http.post("http://localhost:5000/image-api/images", data);
+        check(response, {
+            "status is 201": (r) => r.status === 201,
+        });
+        getCount = 0;
+    });
+    };
 }
