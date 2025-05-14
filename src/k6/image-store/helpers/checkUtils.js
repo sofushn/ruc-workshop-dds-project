39930210c296b353d4b8ca399ad7858eb3808bfa
JacobDNielsen <jacobpeternielsen@gmail.com>
2025-05-14 11:17:05 +0200

    feat: added helper functions for k6 tests

---
+++ src/k6/image-store/helpers/checkUtils.js
@@ -0,0 +1,25 @@
+export function isStatusCode(response, statusCode = 200) {
+    return response.status === statusCode;
+}
+
+export function isResponseUrlList(response){
+    try {
+        const urls = JSON.parse(response.body);
+
+        return Array.isArray(urls) && urls.length > 0 && urls.every(url => typeof url === "string" && url.startsWith("http"));
+        
+    } catch (error) {
+        return false;
+    }
+}
+
+export function isResponseImage(response) {
+    const contentType = response.headers["Content-Type"] || response.headers["content-type"];
+    const contentLength = response.headers["Content-Length"] || response.headers["content-length"];
+
+    return (
+        contentType &&
+        contentType.startsWith("image/") &&
+        contentLength > 0
+    );
+}
