b87b480f6cc62515d1b85747c88424dee414e697
Sofus Hilfling <sofushn@hotmail.dk>
2025-05-22 15:18:06 +0200

    fix: img post not working when replication off (#87)
    * fix(image-store): post not enabled if replication disabled
    
    * fix(image-store): development mode in docker
    
    * chore: better bool logic

--- src/svc/image-store/Utils.cs
+++ src/svc/image-store/Utils.cs
@@ -14,12 +14,14 @@ public static class Utils
 
     public static async Task WriteToDisk(IWebHostEnvironment environment, string fileName, IFormFile file) {
         string filePath = Path.Combine(GetImageFolderPath(environment), $"{fileName}.jpg");
+        Directory.CreateDirectory(GetImageFolderPath(environment));
         using var stream = File.OpenWrite(filePath);
         await file.CopyToAsync(stream);
     }
 
     public static async Task WriteToTempAsync(IWebHostEnvironment environment, string fileName, IFormFile file) {
-         string filePath = Path.Combine(GetTempFolderPath(environment), $"{fileName}.jpg");
+        string filePath = Path.Combine(GetTempFolderPath(environment), $"{fileName}.jpg");
+        Directory.CreateDirectory(GetTempFolderPath(environment));
         using var stream = File.OpenWrite(filePath);
         await file.CopyToAsync(stream);
     }
