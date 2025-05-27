f02628bdb3f8d0f0e98e88c9fa6cfb91f6311a6b
Sofus Hilfling <sofushn@hotmail.dk>
2025-05-27 11:34:31 +0200

    feat: update test (#99)
    * feat: add util files for k6
    
    * feat: update tests to use util funcs
    
    * docs: add some stuff about how test are being run
    
    * feat: update test targets

--- src/k6/README.md
+++ src/k6/README.md
@@ -5,12 +5,44 @@
 
 How to:
 
-Run WINGET command:
-* winget install k6 --source winget
-* Run test script: k6 run NAME_OF_TEST.js
+- Install k6 locally or run through docker (see guide below)
+  - `winget install k6 --source winget`
+- Run test script: `k6 run NAME_OF_TEST.js`
+
+Tests:
+- breakpoint
+- spike
+- stress
+
 
 Save test result:
+```
 $timestamp = Get-Date -Format "yyyyMMdd-HHmmss"; if (!(Test-Path -Path "./results")) { New-Item -ItemType Directory -Path "../results" }; k6 run --summary-export="results/summary-$timestamp.json" --out csv="results/results-$timestamp.csv" ./breakpoint-test.js | Tee-Object -FilePath "results/output-$timestamp.txt"
+```
 
 Optional:
 Remove tag "requestId" from tests, if performance is affected. If kept it will save current VUs ID and their iteration ID, for each request
+
+## Run in docker
+
+```
+cd ./src/k6
+docker run --rm -it --entrypoint /bin/sh -v ${PWD}:/scripts grafana/k6
+```
+
+Navigate to `/scripts` folder and run k6 command.
+
+```
+timestamp=$(date +"%Y%m%d_%H%M%S")
+mkdir results
+timestamp=$(date +"%Y%m%d_%H%M%S") k6 run <test> --summary-export=results/summary-${timestamp}.json --out csv=results/results-${timestamp}.csv --out json=results/results-${timestamp}.json
+
+timestamp=$(date +"%Y%m%d_%H%M%S") k6 run stress.js --summary-export=results/summary-${timestamp}.json --out csv=results/results-${timestamp}.csv --out json=results/results-${timestamp}.json --env BASE_URL=http://192.168.0.2:8080
+```
+
+## Options
+
+| Env name | Possible values | Default
+| --- | --- | --- |
+| `TEST_TYPE` | `GetImageById`, `GetMapById`, `GetWaypointsByMapId`, `PostWaypoint`, `Combined` | `GetImageById` |
+| `BASE_URL` | Any url (including http/https in the beginning and possible port number) | `http://localhost:8080` |
---
+++ src/k6/helpers/endpoints.js
@@ -0,0 +1,73 @@
+import { isResponseImage } from './checkUtils.js';
+
+const TEST_TYPE = __ENV.TEST_TYPE ?? 'GetImageById';
+const IS_COMBINED_TEST = TEST_TYPE == 'Combined';
+const BASE_URL = __ENV.BASE_URL ?? 'http://localhost:8080';
+
+const endpoints = [
+    {
+        name: 'GetImageById',
+        url: `${BASE_URL}/image-api/images/1.jpg`,
+        method: 'GET',
+        checks: {
+            'status is 200': (r) => r.status === 200,
+            'response time < 800ms': (r) => r.timings.duration < 800,
+            'response is an image': (r) => isResponseImage(r),
+        },
+        ratio: 1,
+    },
+    {
+        name: 'GetMapById',
+        url: `${BASE_URL}/api/map/1`,
+        method: 'GET',
+        checks: {
+            'status is 200': (r) => r.status === 200,
+            'response time < 800ms': (r) => r.timings.duration < 800,
+        },
+        ratio: 0.01,
+    },
+    {
+        name: 'GetWaypointsByMapId',
+        url: `${BASE_URL}/api/map/1/waypoints`,
+        method: 'GET',
+        checks: {
+            'status is 200': (r) => r.status === 200,
+            'response time < 800ms': (r) => r.timings.duration < 800,
+        },
+        ratio: 0.05,
+    },
+    {
+        name: 'PostWaypoint',
+        url: `${BASE_URL}/api/waypoint`,
+        method: 'POST',
+        body: {
+            mapId: 1,
+            height: 50.0,
+            filePath: 'test.jpg'
+        },
+        checks: {
+            'status is 201': (r) => r.status === 201,
+            'response time < 1000ms': (r) => r.timings.duration < 1000,
+        },
+        ratio: 0.0001,
+    },
+];
+
+export function getEndpoint(imageId) {
+    endpoints[0].url = `${BASE_URL}/image-api/images/${imageId}.jpg`;
+
+    return IS_COMBINED_TEST 
+        ? pickEndpointWeighted() 
+        : endpoints.find(e => e.name === TEST_TYPE) || endpoints[0];
+}
+
+function pickEndpointWeighted() {
+    const total = endpoints.reduce((sum, e) => sum + e.ratio, 0);
+    let r = Math.random() * total;
+    let cumulative = 0;
+    for (const e of endpoints) {
+        cumulative += e.ratio;
+        if (r < cumulative) return e;
+    }
+    return endpoints[0];
+}
